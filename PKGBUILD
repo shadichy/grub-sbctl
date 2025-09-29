eb8bfa9a36b426900b9812fba68b248311063d44
shadichy <shadichy@blisslabs.org>
2025-09-29 15:33:08 +0700

    Bump new version

--- PKGBUILD
+++ PKGBUILD
@@ -1,28 +1,36 @@
-# Maintainer: shadichy <shadichy.dev@gmail.com>
+#shellcheck shell=bash
+# Maintainer: shadichy <shadichy@blisslabs.org>
+
 pkgbase=grub-secureboot-scripts
 pkgname=$pkgbase
-pkgver=0.0.1
+pkgver=0.0.2
 pkgrel=1
-pkgdesc='Grub SecureBoot scripts to setup and enable SecureBoot using sbctl fully automatic'
-arch=(any)
-march=""
+pkgdesc='GRUB SecureBoot scripts to setup SecureBoot using sbctl with automatic signing of GRUB files and kernel images'
+arch=('any')
+march=''
 url='https://github.com/shadichy/grub-sbctl'
-license=('GPL')
+license=('GPLv3')
 depends=('grub' 'sbctl' 'bash')
 source=('gsb.conf' 'grub-sbctl-sign' 'setup.sh' '91-sign-grub.hook')
-sha512sums=('40e0388f1e2de0eb5f5f67c3eb0853c51463c946c6aecd75bc91649f51610992a4fc3cadf76f852bf40d80f06e735a56a174d19eeb3cf449c6cc19b7dc1fc84c' 
-'2f61539475c65eff8b39e2813f8c932738f1ed5c4c8e4837c180f249522f690780b13957b2b02998c7e35751a4a48bcec4dfebdffbb4f3b7a6186d6de4e92f9d' 
-'65b093f48f6691933c4b5001462c21b8f746829abc6737634d3dbe75de778e06e049de6fc81c984b09713ddbc4847e1e3abf0b5d742494fc2b89f35c0dd054b6' 
-'27bf0637e6c58bee0fadaa5a31074dac0b9aa830ae2bf369174c17388363947ba6936af71dea0d05eeab004419cf1cdb0870ac3c141a7bcd360cd687e610b977')
+sha512sums=(
+	'40e0388f1e2de0eb5f5f67c3eb0853c51463c946c6aecd75bc91649f51610992a4fc3cadf76f852bf40d80f06e735a56a174d19eeb3cf449c6cc19b7dc1fc84c'
+	'41c89c777e50ede4b48296dd2c553cc6b1ec31ab4e66a1dc849fa6fb9655fbd814b664dac425a2ab3ec7fb2e83977a80a1de5ac42d620f1ebf061449b3d6796d'
+	'326aa207da395da46e4fabdb6e3c3848cf81bef3c9998db75d721b9dd56ca7f2961b93e73ae3cecbd4e5c55fa9da72275c4a2601772adbaceec2f44d61da1753'
+	'27bf0637e6c58bee0fadaa5a31074dac0b9aa830ae2bf369174c17388363947ba6936af71dea0d05eeab004419cf1cdb0870ac3c141a7bcd360cd687e610b977'
+)
+
+pkgver() {
+	cat $srcdir/../VERSION
+}
 
 package() {
-    mkdir -p $pkgdir/etc $pkgdir/usr/bin $pkgdir/usr/share/grub/sbctl $pkgdir/usr/share/libalpm/hooks
-    install -dm644 $pkgdir/etc
-    install -dm755 $pkgdir/usr/bin
-    install -dm644 $pkgdir/usr/share/grub/sbctl
-    install -dm644 $pkgdir/usr/share/libalpm/hooks
-    install -m644 'gsb.conf' $pkgdir/etc
-    install -m644 '91-sign-grub.hook' $pkgdir/usr/share/libalpm/hooks
-    install -m755 'grub-sbctl-sign' $pkgdir/usr/bin
-    install -m755 'setup.sh' $pkgdir/usr/share/grub/sbctl
+	mkdir -p $pkgdir/etc $pkgdir/usr/bin $pkgdir/usr/share/grub/sbctl $pkgdir/usr/share/libalpm/hooks
+	install -dm644 $pkgdir/etc
+	install -dm755 $pkgdir/usr/bin
+	install -dm644 $pkgdir/usr/share/grub/sbctl
+	install -dm644 $pkgdir/usr/share/libalpm/hooks
+	install -m644 'gsb.conf' $pkgdir/etc
+	install -m644 '91-sign-grub.hook' $pkgdir/usr/share/libalpm/hooks
+	install -m755 'grub-sbctl-sign' $pkgdir/usr/bin
+	install -m755 'setup.sh' $pkgdir/usr/share/grub/sbctl
 }
--- setup.sh
+++ setup.sh
@@ -1,41 +1,113 @@
 #!/bin/bash
 
+set -euo pipefail
+
+# Ensure the script is run as root
 if [ $EUID != 0 ]; then
 	echo "Insufficient permission!"
+	echo "Please run this script as root"
 	exit 1
 fi
 
-if ! { sbctl status | grep -Eq 'Setup Mode:.+Enabled'; }; then
-	echo "Setup Mode is not enabled!"
-	exit 1
-fi
-
+# Source configuration file(s)
 # shellcheck disable=SC1091
-source /etc/gsb.conf
+if [ -f /etc/gsb.conf ]; then
+	source /etc/gsb.conf
+	if [ -f /etc/gsb.user.conf ]; then
+		source /etc/gsb.user.conf
+	fi
+else
+	source gsb.conf # For debugging purposes
+fi
 
+while [ $# -gt 0 ]; do
+	case $1 in
+	--install-sbctl) INSTALL_SBCTL=true ;;
+	--grub-keydir) GRUB_KEYDIR=$2 && shift ;;
+	--bootloader-path) BL_PATH=$2 && shift ;;
+	--grub-confdir) GRUB_CONFDIR=$2 && shift ;;
+	--grub-modules) GRUB_MODULES=$2 && shift ;;
+	--grub-bootloader-id) BL_ID=$2 && shift ;;
+	--target) TARGET=$2 && shift ;;
+	--write-config) CONFIG_OVERRIDE=true ;;
+	--help | -h)
+		cat <<EOF
+Usage: $0 [OPTIONS]
+
+Options:	
+	--install-sbctl						Install sbctl SecureBoot key if not already installed
+	--grub-keydir PATH				Directory to store GRUB gpg keys
+	--bootloader-path PATH		Path to the EFI System Partition's bootloader
+	--grub-confdir PATH				Directory for GRUB configuration files (where grub.cfg is located)
+	--grub-modules MODULES		GRUB modules to include
+	--grub-bootloader-id ID		GRUB bootloader ID (default: auto-detected)
+	--target TARGET						Target architecture (i386, x86_64, arm, arm64; default: auto-detected)
+	--write-config						Write your custom configuration (/etc/gsb.user.conf)
+	--help 										Show this help message and exit
+
+Default values for GRUB keydir, confdir and modules are specified in /etc/gsb.conf.
+EOF
+		exit 0
+		;;
+	*) echo "Unknown option: $1" && exit 1 ;;
+	esac
+	shift
+done
+
+# Create gpg key for signing GRUB files to prevent security prohibitions
 # shellcheck disable=SC2174
 mkdir --mode 0700 -p "$GRUB_KEYDIR"
 gpg --homedir "$GRUB_KEYDIR" --gen-key
 gpg --homedir "$GRUB_KEYDIR" --export >"$GRUB_KEYDIR/boot.key"
 
-target=$(uname -m)
-case "$target" in
-i?86 | x86) target="i386" ;;
-aarch64 | arm64) target="arm64" ;;
-arm*) target="arm" ;;
-esac
+if [ ! "$TARGET" ]; then
+	TARGET=$(uname -m)
+	case "$TARGET" in
+	i?86 | x86) TARGET="i386" ;;
+	aarch64 | arm64) TARGET="arm64" ;;
+	arm*) TARGET="arm" ;;
+	esac
+fi
 
-bl_id=$(basename "$(dirname "$(find "$BL_PATH" -mindepth 2 -maxdepth 2 -type f -iname "grub*.efi" | head -1)")")
+if [ ! "$BL_ID" ]; then
+	BL_ID=$(basename "$(dirname "$(find "$BL_PATH" -mindepth 2 -maxdepth 2 -type f -iname "grub*.efi" | head -1)")")
+fi
+
+if [ "$CONFIG_OVERRIDE" = true ]; then
+	cat <<EOF >/etc/gsb.user.conf
+GRUB_KEYDIR="$GRUB_KEYDIR"
+BL_PATH="$BL_PATH"
+GRUB_CONFDIR="$GRUB_CONFDIR"
+GRUB_MODULES="$GRUB_MODULES"
+BL_ID="$BL_ID"
+TARGET="$TARGET"
+EOF
+fi
+
+if [ "$INSTALL_SBCTL" = true ]; then
+	if ! { sbctl status | grep -Eq 'Setup Mode:.+Enabled'; }; then
+		echo "Setup Mode is not enabled! Cannot proceed with sbctl key installation."
+		exit 1
+	fi
+
+	# Create sbctl keys, skip if key creted
+	sbctl create-keys || :
+
+	# Enroll keys in the system firmware
+	sbctl enroll-keys -fma
+fi
 
 grub-install \
-	--target="${target}-efi" \
+	--target="${TARGET}-efi" \
 	--efi-directory="$(dirname "$BL_PATH")" \
-	--bootloader-id="$bl_id" \
+	--bootloader-id="$BL_ID" \
 	--boot-directory="$(dirname "$GRUB_CONFDIR")" \
 	--force \
 	--modules="$GRUB_MODULES" \
 	--pubkey="$GRUB_KEYDIR/boot.key" \
 	--sbat=/usr/share/grub/sbat.csv \
 	--no-nvram \
-	--disable-shim-lock &&
-	grub-sbctl-sign || exit 1
+	--disable-shim-lock
+
+# Sign GRUB files and modules
+grub-sbctl-sign
